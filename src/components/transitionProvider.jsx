2113452ddbd9b458a5acfef7de08266d863b57cd
M.Rami Alshaar <rshaar559@gmail.com>
2025-05-11 21:24:48 +0300

    Update gradiant

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -12,7 +12,7 @@ const TransitionProvider = ({ children }) => {
     <AnimatePresence mode="wait">
       <div
         key={pathName}
-        className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-200"
+        className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-300"
       >
         <motion.div
           className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
